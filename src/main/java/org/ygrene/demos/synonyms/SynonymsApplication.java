d36569c19dcef853ad6f8ac48e3fe9b687608a2f
Jim Popadak <jim.popadak@outlook.com>
2017-09-14 19:41:19 -0400

    Basic Spring-Boot Project Commit

---
+++ src/main/java/org/ygrene/demos/synonyms/SynonymsApplication.java
@@ -0,0 +1,12 @@
+package org.ygrene.demos.synonyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynonymsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynonymsApplication.class, args);
+    }
+}
